e29858eaf2e7374e7d0704df3e5e635784dd0bea
89lixx <1034186061@qq.com>
2019-10-07 23:22:26 +0800

    add hw1

---
+++ 井字棋/Assets/welcome.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class welcome : MonoBehaviour
+{
+    public Texture2D img;
+
+    private void OnGUI()
+    {
+        float height = Screen.height * 0.5f;
+        float width = Screen.width * 0.5f;
+
+        //back ground
+        GUIStyle bg = new GUIStyle();
+        bg.normal.background = img;
+        GUI.Label(new Rect(0, 0, Screen.width, Screen.height), "", bg);
+
+
+        GUIStyle st = new GUIStyle {
+            fontSize = 50,
+            fontStyle = FontStyle.Bold,
+        };
+        GUI.Label(new Rect(width-130, 50, height, width), "Tic Tac Toe", st);
+        if(GUI.Button(new Rect(width - 90, 300, 200,100), "PLAYER  VS  PLAYER"))
+        {
+
+            Application.LoadLevel("PLAY");
+        }
+    }
+}
